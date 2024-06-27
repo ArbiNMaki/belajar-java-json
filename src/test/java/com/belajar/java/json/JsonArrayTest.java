43090058528ea40212a3ce75091b278def91016b
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-27 19:52:26 +0700

    Read Json Array

--- src/test/java/com/belajar/java/json/JsonArrayTest.java
+++ src/test/java/com/belajar/java/json/JsonArrayTest.java
@@ -1,9 +1,12 @@
 package com.belajar.java.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class JsonArrayTest {
@@ -16,5 +19,17 @@ public class JsonArrayTest {
         String json = objectMapper.writeValueAsString(hobbies);
 
         System.out.println(json);
+    }
+
+    @Test
+    void testReadJsonArray() throws JsonProcessingException {
+        String json = """
+                ["Coding", "Reading", "Traveling"]
+                """;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> hobbies = objectMapper.readValue(json, new TypeReference<List<String>>() {});
+
+        Assertions.assertEquals(List.of("Coding", "Reading", "Traveling"), hobbies);
     }
 }
