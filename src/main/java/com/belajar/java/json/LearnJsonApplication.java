f0b1524b733e15137ada035c0d8a490b04b38cc5
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-27 17:32:47 +0700

    first

---
+++ src/main/java/com/belajar/java/json/LearnJsonApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.java.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belajar/java/json/LearnJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.java.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
